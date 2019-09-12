52ac91584035acd9f4d9de4485ae9d8650d2e732
Anton Lazarev <antonok35@gmail.com>
2019-09-12 13:38:09 -0700

    allow reporting webcompat issues through the Shields panel

--- components/definitions/chromel.d.ts
+++ components/definitions/chromel.d.ts
@@ -200,6 +200,7 @@ declare namespace chrome.braveShields {
   const getHTTPSEverywhereEnabledAsync: any
   const setNoScriptControlTypeAsync: any
   const getNoScriptControlTypeAsync: any
+  const reportBrokenSite: any
 
   type BraveShieldsViewPreferences = {
     showAdvancedView: boolean
